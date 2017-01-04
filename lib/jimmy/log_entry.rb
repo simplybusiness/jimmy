342acfc158fbeeddea3b70976867e08e5807b0b8
Jhonny E <jhonny.edelist@simplybusiness.co.uk>
2017-01-04 13:21:48 +0000

    refactored error formatting into the entry class, so that it can be used by external code as well

--- lib/jimmy/log_entry.rb
+++ lib/jimmy/log_entry.rb
@@ -3,5 +3,21 @@ module Jimmy
     def <<(hash)
       self.merge!(hash)
     end
+
+    def error(error)
+      self.merge!(attributes_for_error(error))
+    end
+
+    private
+
+    def attributes_for_error(error)
+      {
+          response_code: '500',
+          error_class: error.class.name,
+          error_message: error.message,
+          error_backtrace: error.backtrace.join("\n"),
+      }
+    end
+
   end
 end
