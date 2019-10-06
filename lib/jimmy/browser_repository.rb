0ef92a3283d63bff5266b0c4a40c256f89967006
John Gallagher <john.gallagher@simplybusiness.co.uk>
2019-10-06 09:59:27 +0100

    Clean up

--- lib/jimmy/browser_repository.rb
+++ lib/jimmy/browser_repository.rb
@@ -2,38 +2,32 @@
 
 require 'csv'
 
-CSV::Converters[:array] = ->(value) { 
-  if value.to_s.start_with?('[') && value.to_s.end_with?(']')
-    instance_eval(value)
-  else
-    value
-  end
-}
-
-CSV::Converters[:hash] = ->(value) { 
-  if value.to_s.start_with?('{') && value.to_s.end_with?('}')
-    instance_eval(value)
-  else
-    value
-  end
-}
-
 module Jimmy
   class BrowserRepository
     def initialize(csv:)
-      @browsers = csv.then(&parse).map(&to_browser)
+      @browsers = csv.yield_self(&parse).map(&to_browser)
     end
 
     def find_by(user_agent:)
       @browsers.find { |browser| browser.user_agent == user_agent }
     end
 
+    private
+
     def parse
-      ->(csv) { CSV.parse(csv, headers: true, converters: [:array, :hash]).map(&:to_h) }
+      ->(csv) { CSV.parse(csv, headers: true, converters: [hash_converter, array_converter]).map(&:to_h) }
     end
 
     def to_browser
       ->(attrs) { Browser.new(attrs.symbolize_keys) }
+    end
+
+    def hash_converter
+      ->(value) { value.to_s.start_with?('{') && value.to_s.end_with?('}') ?  instance_eval(value) : value }
+    end
+
+    def array_converter
+      ->(value) { value.to_s.start_with?('[') && value.to_s.end_with?(']') ?  instance_eval(value) : value }
     end
   end
 end
