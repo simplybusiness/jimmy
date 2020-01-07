117812764da267edd98b0ff902571a92418c4c19
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-07 15:05:35 +0000

    gemstash versioning for dependabot

--- lib/jimmy/version.rb
+++ lib/jimmy/version.rb
@@ -1,3 +1,7 @@
 module Jimmy
-  VERSION = '0.4.8'
+  VERSION = '0.4.8' # rubocop:disable Style/MutableConstant
+
+  # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
+  VERSION << '.' << ENV['GEM_PRE_RELEASE'].strip \
+  unless ENV.fetch('GEM_PRE_RELEASE', '').strip.empty?
 end
