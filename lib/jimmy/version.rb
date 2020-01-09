2f439cb08997fd9913c41eb1d6dbcd8d63259c01
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2020-01-09 13:37:45 +0000

    bump version for new gemstash release

--- lib/jimmy/version.rb
+++ lib/jimmy/version.rb
@@ -1,5 +1,5 @@
 module Jimmy
-  base = '0.4.8'
+  base = '0.4.9'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
