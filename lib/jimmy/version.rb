d9770c12abefecc4f55d9f7ea1e4b5d195d6f55c
Nicole Rifkin <rifkinni@users.noreply.github.com>
2020-10-13 13:13:07 -0400

    version check action (#30)
    * version check action
    
    * version

--- lib/jimmy/version.rb
+++ lib/jimmy/version.rb
@@ -1,5 +1,5 @@
 module Jimmy
-  base = '0.4.12'
+  base = '0.4.13'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
