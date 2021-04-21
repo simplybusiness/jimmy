6d7a359a9ccf0ee9971837d80f8e0ab612eaaccd
Adam Leach <adam.leach@simplybusiness.co.uk>
2021-04-21 15:05:41 +0100

    Improve the semaphore caching (#37)
    * Improve the semaphore caching
    
    * bump patch version
    
    Co-authored-by: dobby-app[bot] <78910979+dobby-app[bot]@users.noreply.github.com>

--- lib/jimmy/version.rb
+++ lib/jimmy/version.rb
@@ -1,5 +1,5 @@
 module Jimmy
-  base = '0.4.16'
+  base = '0.4.17'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
